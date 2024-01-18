19a5339a88076ac586fb6496e175b1e607de9bdc
MehediHasan06 <mehedihasanmasud06@gmail.com>
2024-01-18 20:45:38 +0600

    Add new Book component and books data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
+import Book from "src/components/Book"
 import Filter from "src/components/Filter"
 import Navbar from "src/components/Navbar"
 import SearchBar from "src/components/SearchBar"
+import booksData from "/src/assets/books.json"
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main className="my-10 lg:my-14">
+        {/* Header */}
         <header className="mb-8 lg:mb-10 mx-auto max-w-7xl">
           <div className="mx-auto flex items-end justify-between max-md:max-w-[95%] max-md:flex-col max-md:items-start max-md:space-y-4">
             <div>
@@ -14,7 +17,6 @@ export default function Home() {
               <h2 className="mb-6 font-['Playfair_Display'] text-3xl font-bold lg:text-4xl">
                 Trending Books of the Year
               </h2>
-
               {/* search bar */}
               <SearchBar />
             </div>
@@ -25,6 +27,13 @@ export default function Home() {
             </div>
           </div>
         </header>
+
+        {/* Books Grids */}
+        <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {booksData.map((book) => {
+            return <Book key={book.id} book={book} />
+          })}
+        </div>
       </main>
     </>
   )
